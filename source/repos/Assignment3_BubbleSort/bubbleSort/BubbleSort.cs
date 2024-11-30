17adeee68c7ab84c9082dd320d3319ef93779ab8
Irfan <alamandirfan@gmail.com>
2024-11-30 19:09:00 +0545

    Assignment3 committed

---
+++ source/repos/Assignment3_BubbleSort/bubbleSort/BubbleSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubbleSort
+{
+    public class BubbleSort
+    {
+        
+        public void Sort(uint[] array)
+        {
+            int n = array.Length;
+
+            
+            for (int i = 0; i < n - 1; i++)
+            {
+                
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        
+                        uint temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+
+                        Console.WriteLine($"After swapping elements at index {j} and {j + 1}:");
+                        PrintArray(array);
+                    }
+                }
+            }
+        }
+
+        public void PrintArray(uint[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ source/repos/Assignment3_BubbleSort/bubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubbleSort
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            uint[] x = { 5, 6, 8, 2, 4, 0, 1 };
+
+            Console.WriteLine("Initial array:");
+            PrintArray(x);  
+
+           
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.Sort(x);
+
+            Console.WriteLine("Sorted array:");
+            PrintArray(x);
+        }
+        static void PrintArray(uint[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
